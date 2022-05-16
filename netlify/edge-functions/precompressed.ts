6b740407b795880ec8a09b634c319c9248709477
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2022-05-16 14:20:05 +0000

    More direct response control

--- netlify/edge-functions/precompressed.ts
+++ netlify/edge-functions/precompressed.ts
@@ -1,13 +1,20 @@
 import type { Context } from "netlify:edge";
 
 export default async (request: Request, context: Context) => {
+  let url = request.url;
   let acceptEncodingHeader = request.headers.get('Accept-Encoding') || '';
   // ignores quality
   let supportedEncodings = acceptEncodingHeader.split(',').map(v => v.trimStart());
   let supportsBr = supportedEncodings.includes('br');
-  context.log("URL: " + request.url + ", acceptEncodingHeader: " + acceptEncodingHeader + ", supportsBr: " + supportsBr);
+  context.log("URL: " + url + ", acceptEncodingHeader: " + acceptEncodingHeader + ", supportsBr: " + supportsBr);
   if (supportsBr) {
     // ignores search query
-    return context.rewrite(request.url + '.br');
+    let response = await context.rewrite(url + '.br');
+    if (response.status != 200) return response;
+    let data = await response.arrayBuffer();
+    var fixedHeaders = new Headers(response.headers);
+    fixedHeaders.set('Content-Type', 'application/' + (url.endsWith('.js') ? 'javascript' : 'wasm'));
+    fixedHeaders.set('Content-Encoding', 'br');
+    return new Response(data, {headers: fixedHeaders});
   }
 };

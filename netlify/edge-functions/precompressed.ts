b9898d102c3ded33cdd3809513444d9289c3fe51
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2022-06-03 13:52:54 +0000

    Debug netlify caching

--- netlify/edge-functions/precompressed.ts
+++ netlify/edge-functions/precompressed.ts
@@ -5,11 +5,17 @@ export default async (request: Request, context: Context) => {
   let acceptEncodingHeader = request.headers.get('Accept-Encoding') || '';
   // ignores quality
   let supportedEncodings = acceptEncodingHeader.split(',').map(v => v.trimStart());
+  let etag = request.headers.get('If-None-Match') || '';
   let supportsBr = supportedEncodings.includes('br');
-  context.log("URL: " + url + ", acceptEncodingHeader: " + acceptEncodingHeader + ", supportsBr: " + supportsBr);
+  context.log("URL: " + url + "; acceptEncodingHeader: " + acceptEncodingHeader + "; supportsBr: " + supportsBr + "; etag: " + etag);
   if (supportsBr) {
     // ignores search query
     let response = await context.rewrite(url + '.br');
+    let responseEtag = response.headers.get('ETag') || '';
+    context.log("Response etag: " + responseEtag);
+    if (etag.length >= 4 && responseEtag === etag) {
+      console.log("Match; status: " + response.status);
+    }
     if (response.status != 200) return response;
     let data = await response.arrayBuffer();
     var fixedHeaders = new Headers(response.headers);

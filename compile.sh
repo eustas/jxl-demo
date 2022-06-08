cca0e5a52865e316b196a354501de3f0c61242bd
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2022-06-08 12:41:54 +0000

    Fix embedding

--- compile.sh
+++ compile.sh
@@ -10,7 +10,7 @@ jxl_emcc_worker_js=${jxl_emcc_worker_js//"'"/"\\'"}
 
 coi_serviceworker_js=$(cat ./coi-serviceworker.template)
 coi_serviceworker_js=${coi_serviceworker_js//'$jxl_emcc.js$'/$jxl_emcc_js}
-coi_serviceworker_js=${coi_serviceworker_js//'$jxl_emcc_worker.js$'/$jxl_emcc_worker_js}
+coi_serviceworker_js=${coi_serviceworker_js//'$jxl_emcc.worker.js$'/$jxl_emcc_worker_js}
 echo "${coi_serviceworker_js}" | uglifyjs -m -c > ./coi-serviceworker.js
 
 brotli -Zfk ./coi-serviceworker.js

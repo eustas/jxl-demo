f5d7f4abf4a0d8c6bad3cc3cc0a33b6caf63afac
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2022-06-08 11:01:25 +0000

    Fix typo

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ jxl_emcc_worker_js=$(uglifyjs ./compressed/jxl_emcc.worker.js -m -c | sed -z "s/
 coi_serviceworker_js=$(cat ./coi-serviceworker.template)
 coi_serviceworker_js=${coi_serviceworker_js//'$jxl_emcc.js$'/$jxl_emcc_js}
 coi_serviceworker_js=${coi_serviceworker_js//'$jxl_emcc_worker.js$'/$jxl_emcc_worker_js}
-echo "${coi_serviceworker_js}" > ./coi-serviceworker.js
+echo "${coi_serviceworker_js}" | uglifyjs -m -c > ./coi-serviceworker.js
 
 #brotli -Zfk ./coi-serviceworker.js
 #zopfli      ./coi-serviceworker.js

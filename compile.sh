8ebbc0764ea8a0299e6dac9e8edd9c2005ab5c68
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2022-06-08 08:53:12 +0000

    Optimize resource loading

---
+++ compile.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+jxl_emcc_js=$(uglifyjs ./compressed/jxl_emcc.js -m -c | sed -z "s/\n/ /g;s/'/\\\'/g")
+jxl_emcc_worker_js=$(uglifyjs ./compressed/jxl_emcc.worker.js -m -c | sed -z "s/\n/ /g;s/'/\\\'/g")
+
+coi_serviceworker_js=$(cat ./coi-serviceworker.template)
+coi_serviceworker_js=${coi_serviceworker_js//'$jxl_emcc.js$'/$jxl_emcc_js}
+coi_serviceworker_js=${coi_serviceworker_js//'$jxl_emcc_worker.js$'/$jxl_emcc_worker_js}
+echo "${coi_serviceworker_js}" | uglifyjs -m -c > ./compressed/coi_serviceworker.js
+
+brotli -Zfk ./compressed/coi_serviceworker.js
+zopfli      ./compressed/coi_serviceworker.js
+brotli -Zfk ./compressed/jxl_emcc.wasm
+zopfli      ./compressed/jxl_emcc.wasm

756ac76866e8d692c1fe0d89c5a7e1b8424c7217
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2022-11-08 16:54:48 +0000

    update

--- compile.sh
+++ compile.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
-set -x
+set -kx
 
-jxl_emcc_js=$(uglifyjs ./jxl_emcc.js -m -c)
-jxl_emcc_js=${jxl_emcc_js//"\\"/"\\\\"}
-jxl_emcc_js=${jxl_emcc_js//"'"/"\\'"}
+uglifyjs ./jxl_emcc.js -m -c > /tmp/jxl_emcc.js
+uglifyjs ./jxl_emcc.worker.js -m -c > /tmp/jxl_emcc.worker.js
 
-jxl_emcc_worker_js=$(uglifyjs ./jxl_emcc.worker.js -m -c)
-jxl_emcc_worker_js=${jxl_emcc_worker_js//"\\"/"\\\\"}
-jxl_emcc_worker_js=${jxl_emcc_worker_js//"'"/"\\'"}
+python3 <<EOF
+import pathlib
+sq = chr(39)
+bs = chr(92)
+ds = chr(36)
+jxl_emcc_js = pathlib.Path('/tmp/jxl_emcc.js').read_text().strip()
+jxl_emcc_js = jxl_emcc_js.replace(bs, bs + bs).replace(sq, bs + sq)
+jxl_emcc_worker_js = pathlib.Path('/tmp/jxl_emcc.worker.js').read_text().strip()
+jxl_emcc_worker_js = jxl_emcc_worker_js.replace(bs, bs + bs).replace(sq, bs + sq)
+coi_serviceworker_js = pathlib.Path('coi-serviceworker.template').read_text().strip()
+coi_serviceworker_js = coi_serviceworker_js.replace(ds + 'jxl_emcc.js' + ds, jxl_emcc_js)
+coi_serviceworker_js = coi_serviceworker_js.replace(ds + 'jxl_emcc.worker.js' + ds, jxl_emcc_worker_js)
+pathlib.Path('/tmp/coi-serviceworker.js').write_text(coi_serviceworker_js)
+EOF
 
-coi_serviceworker_js=$(cat ./coi-serviceworker.template)
-coi_serviceworker_js=${coi_serviceworker_js//'$jxl_emcc.js$'/$jxl_emcc_js}
-coi_serviceworker_js=${coi_serviceworker_js//'$jxl_emcc.worker.js$'/$jxl_emcc_worker_js}
-echo "${coi_serviceworker_js}" | uglifyjs -m -c > ./coi-serviceworker.js
+cat /tmp/coi-serviceworker.js | uglifyjs -m -c > ./coi-serviceworker.js
 
 brotli -Zfk ./coi-serviceworker.js
 zopfli      ./coi-serviceworker.js

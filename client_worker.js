0560b9c5a75be6950705fee3221b45d290c58d65
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2022-11-25 15:18:40 +0000

    load module

--- client_worker.js
+++ client_worker.js
@@ -3,8 +3,13 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-onmessage = function(event) {
+const onmessage = function(event) {
   console.log(event.data);
 }
 
+const onLoadJxlModule = (module) => {
+  console.log('onLoadJxlModule');
+}
+
 importScripts('jxl_decoder.js');
+JxlCodecModule().then(onLoadJxlModule);

417ac4f37494c7a4874f07ba30247cce9cf6d258
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2022-11-28 14:45:08 +0000

    forward

--- client_worker.js
+++ client_worker.js
@@ -3,12 +3,32 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+let decoder = null;
+
+// Serialize work; plus postpone processing until decoder is ready.
+let workQueue = [];
+
+const processQueue = () => {
+  while (workQueue.length) {
+    const data = workQueue.shift();
+    console.log('CW: decoding: ' + data.id);
+    postMessage({id: data.id, result: 'bad'});
+  }
+}
+
 const onmessage = function(event) {
-  console.log(event.data);
+  const data = event.data;
+  if (data.op == 'decode') {
+    workQueue.push(event.data);
+    processQueue();
+  } else {
+    console.log('CW: unknown op: ' + event.data);
+  }
 }
 
 const onLoadJxlModule = (module) => {
-  console.log('onLoadJxlModule');
+  decoder = module;
+  processQueue();
 }
 
 importScripts('jxl_decoder.js');

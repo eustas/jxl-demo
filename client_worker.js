83ef17cee94f1b1a93a4071076db3bfdc9838aaa
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2022-11-28 14:07:05 +0000

    config

--- client_worker.js
+++ client_worker.js
@@ -13,5 +13,5 @@ const onLoadJxlModule = (module) => {
 
 importScripts('jxl_decoder.js');
 console.log('before construct');
-JxlCodecModule.mainScriptUrlOrBlob = 'https://jxl-demo.netlify.app/jxl_decoder.js';
-JxlCodecModule().then(onLoadJxlModule);
+const config = {mainScriptUrlOrBlob: 'https://jxl-demo.netlify.app/jxl_decoder.js'};
+JxlCodecModule(config).then(onLoadJxlModule);

789e38e5170c64c998165007484aafc1427b0956
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2022-12-09 11:01:23 +0000

    dr

--- shared_worker.js
+++ shared_worker.js
@@ -1,11 +1,25 @@
 console.log("Shared worker created");
 
+let wasmModule = null;
+const recipients = [];
+
+const onWasmModuleReady = (module) => {
+  while (recipients.length) {
+    recipients.pop().postMessage({op: 'wasmModule', wasmModule: wasmModule});
+  }
+};
+
+WebAssembly.compileStreaming(fetch('jxl_decoder.wasm')).then(onWasmModuleReady);
+
 onconnect = function (event) {
   const port = event.ports[0];
   console.log("Shared worker connected");
 
-  port.onmessage = function (e) {
-    console.log("Shared worker got message");
-    // port.postMessage(workerResult);
-  };
+  if (wasmModule) {
+    port.postMessage({op: 'wasmModule', wasmModule: wasmModule});
+  } else {
+    recipients.push(port);
+  }
+
+  // port.onmessage = function (e) { console.log("Shared worker got message"); };
 };
